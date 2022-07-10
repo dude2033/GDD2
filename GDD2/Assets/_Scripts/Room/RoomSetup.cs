587a6c1c3f46e7b16f1cd285c25aac4c39bfcfa8
Philipp Drescher <d.phil@gmx.at>
2022-07-10 18:31:58 +0200

    some scene fixes

--- GDD2/Assets/_Scripts/Room/RoomSetup.cs
+++ GDD2/Assets/_Scripts/Room/RoomSetup.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
 
 public class RoomSetup : MonoBehaviour
 {
     [SerializeField] private List<GameObject> destroyOnSetup = new List<GameObject>();
     [SerializeField] private List<VoiceLineListEntry> voiceLineList = new List<VoiceLineListEntry>();
+    [SerializeField] private InputActionManager inputActionManager;
     public bool setupOnStart = false;
     public bool destroyObjects = true;
 
@@ -15,6 +18,10 @@ public class RoomSetup : MonoBehaviour
         {
             SetupRoom();
         }
+
+        // the toggle is needed to register the objects
+        // inputActionManager.enabled = false;
+        // inputActionManager.enabled = true;
     }
 
     [ContextMenu("Setup Room")]
