2b3a95e9175562d3c93af6e6844e26124c3a9af7
Philipp Drescher <d.phil@gmx.at>
2022-07-22 12:49:59 +0200

    added flying numbers room voice lines

--- GDD2/Assets/_Scenes/Rooms/Room_Battery/Scripts/CableStartAutoDisable.cs
+++ GDD2/Assets/_Scenes/Rooms/Room_Battery/Scripts/CableStartAutoDisable.cs
@@ -4,7 +4,16 @@ public class CableStartAutoDisable : MonoBehaviour
 {
     [SerializeField] private GameObject cableStart;
 
-    private void OnDisable() => cableStart.SetActive(false);
-    private void OnEnable() => cableStart.SetActive(true);
-    
+    private void OnDisable()
+    {
+        if(cableStart != null)
+            cableStart.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        if(cableStart != null)
+            cableStart.SetActive(true);
+    }
+  
 }
